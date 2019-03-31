2cfe740308b72b0b607c2478019416ed330a7052
Steinar Darri Þorgilsson <steinardth@gmail.com>
2019-03-31 22:40:34 +0000

    test byrjad

---
+++ TravelPortal/src/is/hi/TravelPortal/FlightManagerTest.java
@@ -0,0 +1,20 @@
+package is.hi.TravelPortal;
+
+class FlightManagerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchForFlights() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void bookFlight() {
+    }
+}
