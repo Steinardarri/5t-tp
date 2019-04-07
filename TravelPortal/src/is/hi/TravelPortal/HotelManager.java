a5002d1383662924db90984047f0b3032eb6bad5
olgeiringi095 <49107030+olgeiringi095@users.noreply.github.com>
2019-04-07 21:16:32 +0000

    Update HotelManager.java

--- TravelPortal/src/is/hi/TravelPortal/HotelManager.java
+++ TravelPortal/src/is/hi/TravelPortal/HotelManager.java
@@ -1,26 +1,60 @@
-package is.hi.TravelPortal;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TravelPortal;
 
-import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
  * @author Olli
  */
+
+
+
 public class HotelManager {
     
-    private List<Hotel> hotelList = new ArrayList<>();
+    private List<Hotel> hotelList;
+    private HotelController hc = new HotelController();
     
-    public List searchForHotels(/*...*/) {
+    
+    
+    public List searchForHotels(String name, String location, Calendar date, double rating) {
          
-        Hotel hotelAK = new Hotel("hotelAK", "Akureyri", 3.8);
-        hotelList.add(hotelAK);
-        
+        hotelList = hc.search(name, location, date, rating);
         return hotelList;
     }
     
+    
+    public List bookRoom(List<Hotel> hotelList, int[] peopleCount, Calendar[] startDate, Calendar[] endDate) {
+        
+        int hotelCount = hotelList.size();
+        List<hotelBooking> hotelBookings = new ArrayList<>();
+        
+        for(int i = 0; i < hotelCount; i++) {
+            hotel = hotelList.get(i);
+            hotelBookings.add(hc.bookRoom(hotel.getHotelID, peopleCount[i], startDate[i], endDate[i]));
+        }
+        
+        return hotelBookings;
+    }
+    
+    
+    //GETTER&SETTER fyrir hotelList (þarf það?)
+    /*
     public List getHotelList() {
         return hotelList;
     }
+    
+    public void setHotelList(List hotelsList) {
+        hotelList = hotelsList;
+    }
+    */
+    
 }
 
