63a135b05f6e69e9b392596d57132aba85fd267a
olgeiringi095 <49107030+olgeiringi095@users.noreply.github.com>
2019-04-08 22:02:41 +0000

    Update HotelManager.java

--- HotelManager.java
+++ HotelManager.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package TravelPortal;
 
-import TravelPortal.Hotel;
 import java.util.Calendar;
 import java.util.List;
 import java.util.ArrayList;
@@ -15,21 +9,43 @@ import java.util.Arrays;
  *
  * @author Olli
  */
+
+
+
 public class HotelManager {
     
     private List<Hotel> hotelList;
+    private HotelController hc = new HotelController();
     
-    public List searchForHotels(/*...*/) {
+    
+    
+    public ArrayList<Hotel> searchForHotels(String name, String location, Calendar date, double rating) {
          
-        Hotel hotelAK = new Hotel("hotelAK", "Akureyri", 3.8);
-        hotelList.add(hotelAK);
-        
+        hotelList = hc.search(name, location, date, rating);
         return hotelList;
     }
     
+    
+    public int bookRoom(Hotel hotel, int peopleCount, Calendar startDate, Calendar endDate) {
+        
+        Booking hotelBooking = new Booking();
+        
+        hotelBooking = hc.bookRoom(hotel.getHotelID, peopleCount, startDate, endDate);
+        int hotelBookingID = hotelBooking.getId();
+        
+        return hotelBookingID;
+    }
+    
+    
+    //GETTER&SETTER fyrir hotelList (þarf það?)
+    /*
     public List getHotelList() {
         return hotelList;
     }
     
+    public void setHotelList(List hotelsList) {
+        hotelList = hotelsList;
+    }
+    */
+    
 }
-
